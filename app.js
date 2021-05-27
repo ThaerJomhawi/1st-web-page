7f9bd3f6a406207d5186e324fda35d7fc0689089
ThaerJomhawi <>
2021-05-27 12:59:04 +0000

    add app.js combined it with html old file

---
+++ app.js
@@ -0,0 +1,15 @@
+//name of the visitor 
+var userName=prompt('What can we call you?  ');
+alert('Welcome ' + userName+' to MCU');
+
+var superpower=prompt('Whats your super power?  ');
+alert('Thats amazing ');
+
+var mrthaer=prompt('choose a number from 1 to 3')
+if (mrthaer == 1) {
+  alert('You are with the avengers ');
+} else if (mrthaer == 2 ) {
+  alert('you are with Thanos');
+} else {
+  alert('you are with the gardians of the galaxy ');
+}
